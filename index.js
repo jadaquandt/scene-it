414cc89fc7760fcc984ee959293342f916bea3a2
Jada Quandt <jadaquandt@gmail.com>
2020-03-07 13:52:03 -0500

    Finished Step 3

--- index.js
+++ index.js
@@ -7,15 +7,14 @@
 
 document.addEventListener('DOMContentLoaded', function () {
    function renderMovies(movieArray) {
-    console.log(movieArray)
+  
     var movieHtml = movieArray.map(currentMovie => {
          return `
     <div class="card mx-2 my-3 col-2">
         <img id="poster" class="card-img-top" src="${currentMovie.Poster}">
             <div class="card-body">
-                        <h5 id="movieTitle" class="d-flex-fill card-title">${currentMovie.Title}</h5>
-                        <p class="card-text"></p>
-                        <p class="date badge badge-secondary">${currentMovie.Year}</p>
+                        <h5 id="movieTitle" class="d-flex card-title">${currentMovie.Title}</h5>
+                        <span class="date badge badge-secondary">${currentMovie.Year}</span>
             </div>
             <div class="card-footer">
                 <footer id="buttonFooter"><button class="mx-auto btn btn-primary">Add</button></footer>
@@ -26,7 +25,25 @@ document.addEventListener('DOMContentLoaded', function () {
     return movieHtml;
 }
         var moviesContainer = document.getElementById('movies-container');
-        moviesContainer.innerHTML = renderMovies(movieData);
+        var searchBar = document.getElementById('search-form')
+        searchBar.addEventListener('submit', function(e){
+            e.preventDefault();
+            
+            var searchString = e.target.elements[0].value.toUpperCase();
+
+            var newArray = [];
+
+        if (searchString !=""){
+            newArray = movieData.filter(movie => {
+                var movieTitle = movie.Title.toUpperCase();
+
+                return movieTitle.includes(searchString);
+            })}
+            e.target.elements[0].value = "";
+
+        moviesContainer.innerHTML = renderMovies(newArray); 
+    })
+     
 });
 
 
