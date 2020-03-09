92fea3647b4fe22e29a89e61feb47352a7763bd9
Jada Quandt <jadaquandt@gmail.com>
2020-03-09 16:45:19 -0400

    finished watchlist aka step 2 of part 2

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//import Axios from "axios";
+
 //In index.js start off by writing a “document ready” block
 //Inside the “document ready” block, define a function called renderMovies
 //Make the function take one parameter called movieArray
@@ -7,7 +9,7 @@
 
 document.addEventListener('DOMContentLoaded', function () {
    function renderMovies(movieArray) {
-  
+    
     var movieHtml = movieArray.map(currentMovie => {
          return `
     <div class="card mx-2 my-3 col-2">
@@ -17,7 +19,9 @@ document.addEventListener('DOMContentLoaded', function () {
                         <span class="date badge badge-secondary">${currentMovie.Year}</span>
             </div>
             <div class="card-footer">
-                <footer id="buttonFooter"><button class="mx-auto btn btn-primary">Add</button></footer>
+                <footer id="buttonFooter">
+                <button id="addButton" class="mx-auto btn btn-primary" onclick="saveToWatchlist('${currentMovie.imdbID}')" >Add</button>
+                </footer>
             </div>
     </div>`
     }).join("");
@@ -26,9 +30,10 @@ document.addEventListener('DOMContentLoaded', function () {
 }
         var moviesContainer = document.getElementById('movies-container');
         var searchBar = document.getElementById('search-form')
+        
         searchBar.addEventListener('submit', function(e){
             e.preventDefault();
-            
+    
             var searchString = e.target.elements[0].value.toUpperCase();
 
             var newArray = [];
@@ -42,8 +47,33 @@ document.addEventListener('DOMContentLoaded', function () {
             e.target.elements[0].value = "";
 
         moviesContainer.innerHTML = renderMovies(newArray); 
-    })
-     
+})
+
 });
 
+function saveToWatchlist(imdbID){
+//1) saveToWatchlist has a parameter called imdbID which will tell us which movie the user clicked on. We’ll use it to sift through movieData.js to find the relevant movie information.
+//2)In saveToWatchlist, create a variable called movie which will contain the rest of this movie’s data.
+//3)We’ll use the Array prototype method find() to pull the rest of the movie data from data.js:  
+
+    var movie = movieData.find(function(currentMovie){
+        return currentMovie.imdbID == imdbID;
+    });
+//In the next line (still inside saveToWatchlist), pull down the watchlist from local storage
+        var watchlistJSON = localStorage.getItem('watchlist');
+//Parse the watchlist with JSON
+        var watchlist = JSON.parse(watchlistJSON);
+//Use an if-statement to check if the watchlist is null, If it is null, set watchlist to an empty array
+        if (watchlist == null) {
+            watchlist = [];
+        }
+//Push movie into the watchlist
+        watchlist.push(movie);
+//Turn the watchlist back into JSON
+        watchlistJSON = JSON.stringify(watchlist);
+//Save the JSONified watchlist back into local storage
+        localStorage.setItem('watchlist', watchlistJSON);
+
+       // console.log(watchlist)
+}
 
